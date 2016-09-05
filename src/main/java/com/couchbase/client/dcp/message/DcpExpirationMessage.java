a31481f5ae14bb7e7de67a59ba4ff090b050aec8
Sergey Avseyev <sergey.avseyev@gmail.com>
2016-09-05 22:54:09 +0300

    expose key and  cas for expiration message

--- src/main/java/com/couchbase/client/dcp/message/DcpExpirationMessage.java
+++ src/main/java/com/couchbase/client/dcp/message/DcpExpirationMessage.java
@@ -16,6 +16,7 @@
 package com.couchbase.client.dcp.message;
 
 import com.couchbase.client.deps.io.netty.buffer.ByteBuf;
+import com.couchbase.client.deps.io.netty.util.CharsetUtil;
 
 import static com.couchbase.client.dcp.message.MessageUtil.DCP_DELETION_OPCODE;
 import static com.couchbase.client.dcp.message.MessageUtil.DCP_EXPIRATION_OPCODE;
@@ -27,10 +28,17 @@ public enum DcpExpirationMessage {
         return buffer.getByte(0) == MessageUtil.MAGIC_REQ && buffer.getByte(1) == DCP_EXPIRATION_OPCODE;
     }
 
+    public static ByteBuf key(final ByteBuf buffer) {
+        return MessageUtil.getKey(buffer);
+    }
+
+    public static long cas(final ByteBuf buffer) {
+        return MessageUtil.getCas(buffer);
+    }
+
     public static short partition(final ByteBuf buffer) {
         return MessageUtil.getVbucket(buffer);
     }
-
 
     public static long bySeqno(final ByteBuf buffer) {
         return buffer.getLong(MessageUtil.HEADER_SIZE);
@@ -38,6 +46,14 @@ public enum DcpExpirationMessage {
 
     public static long revisionSeqno(final ByteBuf buffer) {
         return buffer.getLong(MessageUtil.HEADER_SIZE + 8);
+    }
+
+    public static String toString(final ByteBuf buffer) {
+        return "ExpirationMessage [key: \"" + key(buffer).toString(CharsetUtil.UTF_8)
+                + "\", vbid: " + partition(buffer)
+                + ", cas: " + cas(buffer)
+                + ", bySeqno: " + bySeqno(buffer)
+                + ", revSeqno: " + revisionSeqno(buffer) + "]";
     }
 
 }
