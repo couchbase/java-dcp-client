couchbase/java-dcp-client

commit 267a355258e4ed7d58a716501600795019cc5cc2

JDCP-150 Get bucket config from KV service instead of Manager
Motivation
----------
The streaming HTTP request that was previously used to fetch the config
puts an undue burden on the server.

Modifications
-------------
Seed node ports must now be KV ports instead of Manager ports.

Fix bootstrap timeout enforcement; previously the millisecond value
was interpreted as seconds, resulting in longer than intended timeouts.

ConfigProvider is replaced by BucketConfigArbiter (through which all
bucket configs flow) and BucketConfigHandler, a DCP channel handler
that intercepts incoming bucket configs.

DcpConnectionHandler makes the initial "get bucket config" request.
BucketConfigHandler processes the response and feeds it to the
BucketConfigArbiter.

If the server is older than 5.5, polling is necessary to check for
new bucket configs. In that case, BucketConfigHandler schedules another
"get bucket config" request after the delay configurable as
"configRefreshInterval" (default 2 seconds).

The client now connects to *all* KV nodes, not just nodes that host
an active partition. This is to work around the issue where a
newly-created bucket may report having zero active partitions.

The client builder methods for specifying bootstrap ports have been
removed, since a single uniform port is not sufficient.

Deprecated the `hostnames` Client builder method in favor of a new
`seedNodes` method that does the same thing, but makes it clear that
you can specify a custom KV port too.

Change-Id: Ic3c572264a687c1dd3541b384b41b6a5f522710a
Reviewed-on: http://review.couchbase.org/c/java-dcp-client/+/124196
Tested-by: David Nault <david.nault@couchbase.com>
Tested-by: Build Bot <build@couchbase.com>
Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

src/main/java/com/couchbase/client/dcp/config/ClientEnvironment.java
@@ -15,6 +15,13 @@
  */
 package com.couchbase.client.dcp.config;
 
+import com.couchbase.client.dcp.ConnectionNameGenerator;
+import com.couchbase.client.dcp.ControlEventHandler;
+import com.couchbase.client.dcp.CredentialsProvider;
+import com.couchbase.client.dcp.DataEventHandler;
+import com.couchbase.client.dcp.SystemEventHandler;
+import com.couchbase.client.dcp.buffer.PersistedSeqnos;
+import com.couchbase.client.dcp.buffer.StreamEventBuffer;
 import com.couchbase.client.dcp.core.env.CoreScheduler;
 import com.couchbase.client.dcp.core.env.NetworkResolution;
 import com.couchbase.client.dcp.core.env.resources.NoOpShutdownHook;
@@ -23,19 +30,12 @@ import com.couchbase.client.dcp.core.event.CouchbaseEvent;
 import com.couchbase.client.dcp.core.event.EventBus;
 import com.couchbase.client.dcp.core.event.EventType;
 import com.couchbase.client.dcp.core.time.Delay;
-import com.couchbase.client.dcp.ConnectionNameGenerator;
-import com.couchbase.client.dcp.ControlEventHandler;
-import com.couchbase.client.dcp.CredentialsProvider;
-import com.couchbase.client.dcp.DataEventHandler;
-import com.couchbase.client.dcp.SystemEventHandler;
-import com.couchbase.client.dcp.buffer.PersistedSeqnos;
-import com.couchbase.client.dcp.buffer.StreamEventBuffer;
-import com.couchbase.client.dcp.core.env.ConfigParserEnvironment;
-import com.couchbase.client.dcp.core.node.MemcachedHashingStrategy;
 import com.couchbase.client.dcp.events.DefaultDcpEventBus;
 import io.netty.channel.EventLoopGroup;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.GenericFutureListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import rx.Completable;
 import rx.CompletableSubscriber;
 import rx.Observable;
@@ -44,6 +44,7 @@ import rx.Subscriber;
 import rx.Subscription;
 
 import java.security.KeyStore;
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -53,17 +54,18 @@ import static java.util.Objects.requireNonNull;
  * The {@link ClientEnvironment} is responsible to carry various configuration and
  * state information throughout the lifecycle.
  */
-public class ClientEnvironment implements SecureEnvironment, ConfigParserEnvironment {
-  public static final long DEFAULT_BOOTSTRAP_TIMEOUT = TimeUnit.SECONDS.toMillis(5);
+public class ClientEnvironment implements SecureEnvironment {
+  private static final Logger log = LoggerFactory.getLogger(ClientEnvironment.class);
+
+  public static final Duration DEFAULT_BOOTSTRAP_TIMEOUT = Duration.ofSeconds(5);
+  public static final Duration DEFAULT_CONFIG_REFRESH_INTERVAL = Duration.ofSeconds(2);
   public static final long DEFAULT_CONNECT_TIMEOUT = TimeUnit.SECONDS.toMillis(10);
-  public static final Delay DEFAULT_CONFIG_PROVIDER_RECONNECT_DELAY = Delay.linear(TimeUnit.SECONDS, 10, 1);
-  public static final int DEFAULT_CONFIG_PROVIDER_RECONNECT_MAX_ATTEMPTS = Integer.MAX_VALUE;
   public static final long DEFAULT_SOCKET_CONNECT_TIMEOUT = TimeUnit.SECONDS.toMillis(1);
   public static final Delay DEFAULT_DCP_CHANNELS_RECONNECT_DELAY = Delay.fixed(200, TimeUnit.MILLISECONDS);
   public static final int DEFAULT_DCP_CHANNELS_RECONNECT_MAX_ATTEMPTS = Integer.MAX_VALUE;
   public static final boolean DEFAULT_SSL_ENABLED = false;
-  public static final int BOOTSTRAP_HTTP_DIRECT_PORT = 8091;
-  public static final int BOOTSTRAP_HTTP_SSL_PORT = 18091;
+  private static final int DEFAULT_KV_PORT = 11210;
+  private static final int DEFAULT_KV_TLS_PORT = 11207;
 
   /**
    * Stores the list of bootstrap nodes (where the cluster is).
@@ -91,9 +93,14 @@ public class ClientEnvironment implements SecureEnvironment, ConfigParserEnviron
   private final CredentialsProvider credentialsProvider;
 
   /**
-   * Time in milliseconds to wait for initial configuration during bootstrap.
+   * Time to wait for initial configuration during bootstrap.
    */
-  private final long bootstrapTimeout;
+  private final Duration bootstrapTimeout;
+
+  /**
+   * Polling interval when server does not support clustermap change notifications.
+   */
+  private final Duration configRefreshInterval;
 
   /**
    * Time in milliseconds to wait configuration provider socket to connect.
@@ -154,16 +161,6 @@ public class ClientEnvironment implements SecureEnvironment, ConfigParserEnviron
   /**
    * Delay strategy for configuration provider reconnection.
    */
-  private final Delay configProviderReconnectDelay;
-
-  /**
-   * Maximum number of attempts to reconnect configuration provider before giving up.
-   */
-  private final int configProviderReconnectMaxAttempts;
-
-  /**
-   * Delay strategy for configuration provider reconnection.
-   */
   private final Delay dcpChannelsReconnectDelay;
 
   /**
@@ -179,8 +176,6 @@ public class ClientEnvironment implements SecureEnvironment, ConfigParserEnviron
   private final String sslKeystoreFile;
   private final String sslKeystorePassword;
   private final KeyStore sslKeystore;
-  private final int bootstrapHttpDirectPort;
-  private final int bootstrapHttpSslPort;
 
   /**
    * Creates a new environment based on the builder.
@@ -192,14 +187,13 @@ public class ClientEnvironment implements SecureEnvironment, ConfigParserEnviron
     bucket = builder.bucket;
     credentialsProvider = builder.credentialsProvider;
     bootstrapTimeout = builder.bootstrapTimeout;
+    configRefreshInterval = builder.configRefreshInterval;
     connectTimeout = builder.connectTimeout;
     dcpControl = builder.dcpControl;
     eventLoopGroup = builder.eventLoopGroup;
     eventLoopGroupIsPrivate = builder.eventLoopGroupIsPrivate;
     bufferAckWatermark = builder.bufferAckWatermark;
     poolBuffers = builder.poolBuffers;
-    configProviderReconnectDelay = builder.configProviderReconnectDelay;
-    configProviderReconnectMaxAttempts = builder.configProviderReconnectMaxAttempts;
     socketConnectTimeout = builder.socketConnectTimeout;
     dcpChannelsReconnectDelay = builder.dcpChannelsReconnectDelay;
     dcpChannelsReconnectMaxAttempts = builder.dcpChannelsReconnectMaxAttempts;
@@ -213,8 +207,6 @@ public class ClientEnvironment implements SecureEnvironment, ConfigParserEnviron
       this.schedulerShutdownHook = scheduler;
       eventBus = new DefaultDcpEventBus(scheduler);
     }
-    bootstrapHttpDirectPort = builder.bootstrapHttpDirectPort;
-    bootstrapHttpSslPort = builder.bootstrapHttpSslPort;
     sslEnabled = builder.sslEnabled;
     sslKeystoreFile = builder.sslKeystoreFile;
     sslKeystorePassword = builder.sslKeystorePassword;
@@ -367,9 +359,16 @@ public class ClientEnvironment implements SecureEnvironment, ConfigParserEnviron
   }
 
   /**
+   * Polling interval when server does not support clustermap change notifications.
+   */
+  public Duration configRefreshInterval() {
+    return configRefreshInterval;
+  }
+
+  /**
    * Time in milliseconds to wait for first configuration during bootstrap.
    */
-  public long bootstrapTimeout() {
+  public Duration bootstrapTimeout() {
     return bootstrapTimeout;
   }
 
@@ -435,20 +434,6 @@ public class ClientEnvironment implements SecureEnvironment, ConfigParserEnviron
   }
 
   /**
-   * Delay strategy for configuration provider reconnection.
-   */
-  public Delay configProviderReconnectDelay() {
-    return configProviderReconnectDelay;
-  }
-
-  /**
-   * Maximum number of attempts to reconnect configuration provider before giving up.
-   */
-  public int configProviderReconnectMaxAttempts() {
-    return configProviderReconnectMaxAttempts;
-  }
-
-  /**
    * Socket connect timeout in milliseconds.
    */
   public long socketConnectTimeout() {
@@ -460,14 +445,6 @@ public class ClientEnvironment implements SecureEnvironment, ConfigParserEnviron
    */
   public EventBus eventBus() {
     return eventBus;
-  }
-
-  public int bootstrapHttpDirectPort() {
-    return bootstrapHttpDirectPort;
-  }
-
-  public int bootstrapHttpSslPort() {
-    return bootstrapHttpSslPort;
   }
 
   @Override
@@ -490,33 +467,22 @@ public class ClientEnvironment implements SecureEnvironment, ConfigParserEnviron
     return sslKeystore;
   }
 
-  @Override
-  public MemcachedHashingStrategy memcachedHashingStrategy() {
-    // This is hardcoded, because memcached nodes do not support DCP anyway.
-    return (info, repetition) -> {
-      throw new UnsupportedOperationException("Can't stream from memcached buckets.");
-    };
-  }
-
   public static class Builder {
     private List<HostAndPort> clusterAt;
     private NetworkResolution networkResolution = NetworkResolution.AUTO;
     private ConnectionNameGenerator connectionNameGenerator;
     private String bucket;
     private CredentialsProvider credentialsProvider;
-    private long bootstrapTimeout = DEFAULT_BOOTSTRAP_TIMEOUT;
+    private Duration configRefreshInterval = DEFAULT_CONFIG_REFRESH_INTERVAL;
+    private Duration bootstrapTimeout = DEFAULT_BOOTSTRAP_TIMEOUT;
     private long connectTimeout = DEFAULT_CONNECT_TIMEOUT;
     private DcpControl dcpControl;
     private EventLoopGroup eventLoopGroup;
     private boolean eventLoopGroupIsPrivate;
     private boolean poolBuffers;
-    private Delay configProviderReconnectDelay = DEFAULT_CONFIG_PROVIDER_RECONNECT_DELAY;
-    private int configProviderReconnectMaxAttempts = DEFAULT_CONFIG_PROVIDER_RECONNECT_MAX_ATTEMPTS;
     private long socketConnectTimeout = DEFAULT_SOCKET_CONNECT_TIMEOUT;
     private Delay dcpChannelsReconnectDelay = DEFAULT_DCP_CHANNELS_RECONNECT_DELAY;
     private int dcpChannelsReconnectMaxAttempts = DEFAULT_DCP_CHANNELS_RECONNECT_MAX_ATTEMPTS;
-    private int bootstrapHttpDirectPort = BOOTSTRAP_HTTP_DIRECT_PORT;
-    private int bootstrapHttpSslPort = BOOTSTRAP_HTTP_SSL_PORT;
 
     private int bufferAckWatermark;
     private EventBus eventBus;
@@ -557,23 +523,18 @@ public class ClientEnvironment implements SecureEnvironment, ConfigParserEnviron
     }
 
 
-    public Builder setBootstrapTimeout(long bootstrapTimeout) {
+    public Builder setBootstrapTimeout(Duration bootstrapTimeout) {
       this.bootstrapTimeout = bootstrapTimeout;
+      return this;
+    }
+
+    public Builder setConfigRefreshInterval(Duration configRefreshInterval) {
+      this.configRefreshInterval = configRefreshInterval;
       return this;
     }
 
     public Builder setConnectTimeout(long connectTimeout) {
       this.connectTimeout = connectTimeout;
-      return this;
-    }
-
-    public Builder setConfigProviderReconnectDelay(Delay configProviderReconnectDelay) {
-      this.configProviderReconnectDelay = configProviderReconnectDelay;
-      return this;
-    }
-
-    public Builder setConfigProviderReconnectMaxAttempts(int configProviderReconnectMaxAttempts) {
-      this.configProviderReconnectMaxAttempts = configProviderReconnectMaxAttempts;
       return this;
     }
 
@@ -615,24 +576,6 @@ public class ClientEnvironment implements SecureEnvironment, ConfigParserEnviron
 
     public Builder setEventBus(EventBus eventBus) {
       this.eventBus = eventBus;
-      return this;
-    }
-
-    /**
-     * If SSL not enabled, sets the port to use for HTTP bootstrap
-     * (default value {@value #BOOTSTRAP_HTTP_DIRECT_PORT}).
-     */
-    public Builder setBootstrapHttpDirectPort(final int bootstrapHttpDirectPort) {
-      this.bootstrapHttpDirectPort = bootstrapHttpDirectPort;
-      return this;
-    }
-
-    /**
-     * If SSL enabled, sets the port to use for HTTP bootstrap
-     * (default value {@value #BOOTSTRAP_HTTP_SSL_PORT}).
-     */
-    public Builder setBootstrapHttpSslPort(final int bootstrapHttpSslPort) {
-      this.bootstrapHttpSslPort = bootstrapHttpSslPort;
       return this;
     }
 
@@ -688,11 +631,19 @@ public class ClientEnvironment implements SecureEnvironment, ConfigParserEnviron
     }
 
     public ClientEnvironment build() {
-      int defaultConfigPort = sslEnabled ? bootstrapHttpSslPort : bootstrapHttpDirectPort;
+      final int defaultKvPort = sslEnabled ? DEFAULT_KV_TLS_PORT : DEFAULT_KV_PORT;
       for (int i = 0; i < clusterAt.size(); i++) {
         HostAndPort node = clusterAt.get(i);
+
+        if (node.port() == 8091 || node.port() == 18091) {
+          log.warn("Seed node '{}' uses port '{}' which is likely incorrect." +
+                  " This should be the port of the KV service, not the Manager service." +
+                  " If the connection fails, omit the port so the client can supply the correct default.",
+              node.host(), node.port());
+        }
+
         if (node.port() == 0) {
-          clusterAt.set(i, node.withPort(defaultConfigPort));
+          clusterAt.set(i, node.withPort(defaultKvPort));
         }
       }
       return new ClientEnvironment(this);
@@ -748,6 +699,7 @@ public class ClientEnvironment implements SecureEnvironment, ConfigParserEnviron
         ", bufferAckWatermark=" + bufferAckWatermark +
         ", connectTimeout=" + connectTimeout +
         ", bootstrapTimeout=" + bootstrapTimeout +
+        ", configRefreshInterval=" + configRefreshInterval +
         ", sslEnabled=" + sslEnabled +
         ", sslKeystoreFile='" + sslKeystoreFile + '\'' +
         ", sslKeystorePassword=" + (sslKeystorePassword != null && !sslKeystorePassword.isEmpty()) +

examples/src/main/java/com/couchbase/client/dcp/examples/AirportsInFrance.java
@@ -41,7 +41,7 @@ public class AirportsInFrance {
 
     // Connect to localhost and use the travel-sample bucket
     final Client client = Client.builder()
-        .hostnames("localhost")
+        .seedNodes("localhost")
         .bucket("travel-sample")
         .credentials("Administrator", "password")
         .build();

examples/src/main/java/com/couchbase/client/dcp/examples/CountDocumentSizes.java
@@ -43,7 +43,7 @@ public class CountDocumentSizes {
 
     // Connect to localhost and use the travel-sample bucket
     final Client client = Client.builder()
-        .hostnames("localhost")
+        .seedNodes("localhost")
         .bucket("travel-sample")
         .credentials("Administrator", "password")
         .build();

examples/src/main/java/com/couchbase/client/dcp/examples/FlowControl.java
@@ -38,7 +38,7 @@ public class FlowControl {
   public static void main(String[] args) throws Exception {
     // Connect to localhost and use the travel-sample bucket
     final Client client = Client.builder()
-        .hostnames("localhost")
+        .seedNodes("localhost")
         .bucket("travel-sample")
         .credentials("Administrator", "password")
         .controlParam(DcpControl.Names.CONNECTION_BUFFER_SIZE, 10000) // set the buffer to 10K

examples/src/main/java/com/couchbase/client/dcp/examples/HighLevelApi.java
@@ -60,7 +60,7 @@ public class HighLevelApi {
 
     try (Client client = Client.builder()
         .credentials("Administrator", "password")
-        .hostnames("localhost")
+        .seedNodes("localhost")
         .bucket(bucketName)
         .flowControl(64 * BYTES_PER_MEGABYTE)
         .userAgent("HighLevelApiExample", "0.1", "bucket:" + bucketName)

examples/src/main/java/com/couchbase/client/dcp/examples/PrintIncomingChanges.java
@@ -46,7 +46,7 @@ public class PrintIncomingChanges {
 
     // Connect to localhost and use the travel-sample bucket
     final Client client = Client.builder()
-        .hostnames("localhost")
+        .seedNodes("localhost")
         .bucket("travel-sample")
         .credentials("Administrator", "password")
         .build();

examples/src/main/java/com/couchbase/client/dcp/examples/StatePersistence.java
@@ -48,7 +48,7 @@ public class StatePersistence {
   public static void main(String[] args) throws Exception {
     // Connect to localhost and use the travel-sample bucket
     final Client client = Client.builder()
-        .hostnames("localhost")
+        .seedNodes("localhost")
         .bucket(BUCKET)
         .credentials("Administrator", "password")
         .build();

integration-test/src/main/java/com/couchbase/client/dcp/test/agent/Application.java
@@ -19,6 +19,8 @@ package com.couchbase.client.dcp.test.agent;
 import com.couchbase.client.java.CouchbaseCluster;
 import com.couchbase.client.java.cluster.ClusterManager;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.util.ResourceLeakDetector;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -28,6 +30,10 @@ import static com.github.therapi.jackson.ObjectMappers.newLenientObjectMapper;
 
 @SpringBootApplication
 public class Application {
+  static {
+    ResourceLeakDetector.setLevel(ResourceLeakDetector.Level.PARANOID);
+  }
+
   private final String username;
   private final String password;
   private final String bootstrapHostnames;

commit e58c738fb00b523b33b42661e9eae3115286d381

Fix UnsupprtedOperationException when using default seed node
Modifications
-------------
Ensure the seed node list is modifiable before attempting to modify it.

This is a regression introduced in 267a355

Change-Id: Ib07c3bf6754a9ce7ddb2289ea256295439caec3a
Reviewed-on: http://review.couchbase.org/c/java-dcp-client/+/124846
Tested-by: Build Bot <build@couchbase.com>
Reviewed-by: David Nault <david.nault@couchbase.com>

src/main/java/com/couchbase/client/dcp/config/ClientEnvironment.java
@@ -45,6 +45,7 @@ import rx.Subscription;
 
 import java.security.KeyStore;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -632,6 +633,8 @@ public class ClientEnvironment implements SecureEnvironment {
 
     public ClientEnvironment build() {
       final int defaultKvPort = sslEnabled ? DEFAULT_KV_TLS_PORT : DEFAULT_KV_PORT;
+      this.clusterAt = new ArrayList<>(clusterAt); // ensure modifiable
+
       for (int i = 0; i < clusterAt.size(); i++) {
         HostAndPort node = clusterAt.get(i);
 